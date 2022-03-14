1ef56bf55b742ed2e20c2546007ede008320cc1a
omda <eabdulwahid@wallpostdev.com>
2022-03-14 22:03:26 +0200

    design answers table and renaming the answers column in the questions table to answers_count

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -34,7 +34,7 @@ class Question extends Model
     }
 
     public function getStatusAttribute() {
-        if ($this->answers > 0) {
+        if ($this->answers_count > 0) {
             if($this->best_answer_id) {
                 return "answered-accepted";
             }
@@ -43,6 +43,10 @@ class Question extends Model
         }
 
         return "unanswered";
+    }
+
+    public function answers() {
+        return $this->hasMany(Answer::class);
     }
 
     // public function getBodyHtmlAtrribute() {
